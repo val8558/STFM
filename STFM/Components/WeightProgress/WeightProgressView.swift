d155e4230e556d64b6d2a48ca770c837c02227ae
Valmir <111082887+val8558@users.noreply.github.com>
2024-12-09 15:01:31 -0300

    MyMetrics menu start

---
+++ STFM/Components/WeightProgress/WeightProgressView.swift
@@ -0,0 +1,18 @@
+//
+//  WeightProgressView.swift
+//  STFM
+//
+//  Created by Valmir Garcia on 09/12/24.
+//
+
+import SwiftUI
+
+struct WeightProgressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeightProgressView()
+}
---
+++ STFM/Views/MyMetricsView.swift
@@ -0,0 +1,18 @@
+//
+//  MyMetricsView.swift
+//  STFM
+//
+//  Created by Valmir Garcia on 09/12/24.
+//
+
+import SwiftUI
+
+struct MyMetricsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyMetricsView()
+}
