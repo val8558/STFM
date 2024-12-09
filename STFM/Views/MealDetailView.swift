cb87322799493feb2f123393df8e8668d2f37a5f
Valmir <111082887+val8558@users.noreply.github.com>
2024-12-09 09:56:04 -0300

    MealDetailView Finished

---
+++ STFM/Views/MealDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MealDetailView.swift
+//  STFM
+//
+//  Created by Valmir Garcia on 06/12/24.
+//
+
+import SwiftUI
+
+struct MealDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MealDetailView()
+}
