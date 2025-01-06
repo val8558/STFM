5cb79abf50822bbef8e1a9f8c86718fdb82e8f0d
Valmir <111082887+val8558@users.noreply.github.com>
2025-01-06 11:34:50 -0300

    Last Update

---
+++ STFM/Views/MetricsViewDetail.swift
@@ -0,0 +1,18 @@
+//
+//  MetricsViewDetail.swift
+//  STFM
+//
+//  Created by Valmir Garcia on 12/12/24.
+//
+
+import SwiftUI
+
+struct MetricsViewDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MetricsViewDetail()
+}
